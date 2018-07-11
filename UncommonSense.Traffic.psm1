3b0e1f63644dc0d4bef7d307d7417aa7c34494f4
Jan Hoek <github@uncommonsense.nl>
2018-07-11 18:50:03 +0200

    Standardized module

--- UncommonSense.Traffic.psm1
+++ UncommonSense.Traffic.psm1
@@ -1,1 +1,14 @@
-. (Join-Path -Path $PSScriptRoot -ChildPath ./Get-RoadObstruction.ps1)
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+Foreach ($import in @($Public + $Private))
+{
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
