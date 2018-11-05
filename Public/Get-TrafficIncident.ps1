b7ef88024d38bfe1731f033b6e8120b7af69e662
Jan Hoek <github@uncommonsense.nl>
2018-11-05 17:02:24 +0100

    Added traffic incidents

--- Public/Get-TrafficIncident.ps1
+++ Public/Get-TrafficIncident.ps1
@@ -8,14 +8,35 @@ function Get-TrafficIncident
 
     $Body = @{}
     $Body.apikey = 'QYUEE3fEcFD7SGMJ6E7QBCMzdQGqRkAi'
-    $Body.polylines = 'true'
-    $Body.polylineBounds = 'true'
-    $Body.totals = 'true'
+    $Body.polylines = 'false'
+    $Body.polylineBounds = 'false'
+    $Body.totals = 'false'
     
     Invoke-RestMethod -Uri https://api.anwb.nl/v1/incidents -Body $Body |
         Select-object -ExpandProperty Roads |
         Where-Object { (-not $RoadNo) -or ($RoadNo -contains $_.road) } |
-        Select-Object -ExpandProperty Segments | gm
-}
+        Select-Object -ExpandProperty Segments | 
+        Where-Object { $_ | Get-Member jams } | 
+        ForEach-Object {
+        $CurrentSegment = $_
 
-Get-TrafficIncident -RoadNo A58, A13
+        $CurrentSegment |
+            Select-Object -ExpandProperty jams | 
+            ForEach-Object {
+            $CurrentIncident = $_
+
+            $Properties = @{
+                RoadNo      = $CurrentIncident.Road
+                SegmentFrom = $CurrentSegment.Start
+                SegmentTo   = $CurrentSegment.End
+                JamFrom     = $CurrentIncident.From
+                JamTo       = $CurrentIncident.To
+                Delay       = [timespan]::FromSeconds($CurrentIncident.Delay)
+                Distance    = $CurrentIncident.Distance
+                PSTypeName  = 'UncommonSense.Traffic.TrafficIncident'
+            }
+                
+            [pscustomobject]$Properties
+        }
+    }
+}
